9ee7c7094dfee61f04a12b0bd36c2598bb635a64
Shinusei <lyshagrishin@gmail.com>
2024-02-14 18:06:32 +0300

    task2

--- dopusk/task2.py
+++ dopusk/task2.py
@@ -1,9 +1,9 @@
-'''from math import log2
+from math import log2
 
 
 def main(y):
     if y < 81:
-        return 46 - y**6/39 - 71(y**2 + y**3/91 + 8*y)**4
+        return 46 - y**6/39 - 71*(y**2 + y**3/91 + 8*y)**4
     if y < 141:
         return y**3
     if y < 211:
@@ -13,12 +13,24 @@ def main(y):
     else:
         return 1 - 69*y**2 - y**3/91
 
-import math'''
-def f(x):
-    print(250000)
-
-d = {250 : f, "2" : "second_funk", "3" : "third_funk"}
+import math
 
 
+def main(y):
+    a = 46 - y**6/39 - 71(y**2 + y**3/91 + 8*y)**4 if y < 81 \
+           else y**3 if y < 141 \
+           else 30*math.log2(y**2 - 1 - y)**5 if y < 211 else y**3 + 1 + y**12
+    a = 1 - 69*y**2 - y**3/91 if y > 279 else a
+    return a
 
-d[250](250)
+
+from math import log2
+
+
+def main(y):
+    a = [46 - y**6 / 39 - 71 * (y**2 + y**3 / 91 + 8 * y) ** 4, y**3][y > 81]
+    a = [a, 30 * log2(y**2 - 1 - y) ** 5][y > 141]
+    a = [a, y**3 + 1 + y**12][y > 211]
+    a = [a, 1 - 69 * y**2 - y**3 / 91][y > 279]
+    return a
+   
