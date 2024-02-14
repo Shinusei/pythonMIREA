62bc26cf2daac66b97b635db9b09efc1f665ed11
Shinusei <lyshagrishin@gmail.com>
2024-02-15 00:23:33 +0300

    task3

--- dopusk/task3.py
+++ dopusk/task3.py
@@ -36,17 +36,21 @@ def main(n, b, m, p):
     return res
 
 
-def main(n, b, m, p, i=1, j=1, c=1, s=0):
-    if j <= b:
-        if i <= m:
-            if c <= n:
-                s = (37 * j**2 - 66 * i**3) ** 6 + 50 * (
-                    34 * p + 86 * k**2 + 41
-                ) ** 3
-                s = main(n, m, b, i, j, c + 1, s)
-            s = main(n, m, b, i + 1, j, 1, s)
-        s = main(n, m, b, 1, j + 1, 1, s)
-    return s
+def main(n, b, m, p, i=1, j=1, k=1):
+    if j > n:
+        j = 1
+        i += 1
+    if i > b:
+        i = 1
+        k += 1
+    if k > m:
+        return 0
+    return (
+        (37 * j**2 - 66 * i**3) ** 6
+        + 50 * (34 * p + 86 * k**2 + 41) ** 3
+        + main(n, b, m, p, i, j+1, k)
+    )
+ 
 
 
 def main(n, b, m, p):
