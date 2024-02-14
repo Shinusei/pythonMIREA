061cca2c3685bf5f0a671ac6d25b6a6ee5cddbf5
Shinusei <lyshagrishin@gmail.com>
2024-02-14 23:47:09 +0300

    task3

--- dopusk/task3.py
+++ dopusk/task3.py
@@ -1,4 +1,4 @@
-""" def main(n, b, m, p):
+def main(n, b, m, p):
     res = 0
     for k in range(1, m + 1):
         for i in range(1, b + 1):
@@ -7,14 +7,53 @@
                     34 * p + 86 * k**2 + 41
                 ) ** 3
     return res
- """
+
 
 def main(n, b, m, p):
-    res = [(37 * j**2 - 66 * i**3) ** 6 + 50 * (
+    res = [
+        (37 * j**2 - 66 * i**3) ** 6 + 50 * (34 * p + 86 * k**2 + 41) ** 3
+        for k in range(1, m + 1)
+        for i in range(1, b + 1)
+        for j in range(1, n + 1)
+    ]
+    return sum(res)
+
+
+def main(n, b, m, p):
+    k = 1
+    res = 0
+    while k <= m:
+        i = 1
+        while i <= b:
+            j = 1
+            while j <= n:
+                res += (37 * j**2 - 66 * i**3) ** 6 + 50 * (
                     34 * p + 86 * k**2 + 41
-                ) ** 3 for k in range(1, m + 1) \
-                    for i in range(1, b + 1) for j in range(1, n + 1)]
-    print (type(res))
+                ) ** 3
+                j += 1
+            i += 1
+        k += 1
     return res
 
-print(main(3, 8, 6, -0.11))
+
+def main(n, b, m, p, i=1, j=1, c=1, s=0):
+    if j <= b:
+        if i <= m:
+            if c <= n:
+                s = (37 * j**2 - 66 * i**3) ** 6 + 50 * (
+                    34 * p + 86 * k**2 + 41
+                ) ** 3
+                s = main(n, m, b, i, j, c + 1, s)
+            s = main(n, m, b, i + 1, j, 1, s)
+        s = main(n, m, b, 1, j + 1, 1, s)
+    return s
+
+
+def main(n, b, m, p):
+    res = (
+        (37 * j**2 - 66 * i**3) ** 6 + 50 * (34 * p + 86 * k**2 + 41) ** 3
+        for k in range(1, m + 1)
+        for i in range(1, b + 1)
+        for j in range(1, n + 1)
+    )
+    return sum(res)
