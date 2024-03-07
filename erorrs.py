36467d2b659803d5b80ef759e4b212210fdf1482
Grishin <lyshagrishin@gmail.com>
2024-03-07 09:16:47 +0300

    crypt pract

---
+++ erorrs.py
@@ -0,0 +1,260 @@
+import math
+
+                    
+for     i in (1 , 4,5) :
+    for   i in (1 , 4,     5 )          
+        print ( i+1 /   4   )   
+           
+def I   (c      =    v):
+  return
+   
+    bmmjhzxbzvbd,z
+ImportError E101
+def get_name(self):
+    if self.first_name and self.last_name:
+        return self.first_name + ' ' + self.last_name
+    else:
+        return self.last_name
+
+class User(object):
+  def __init__(self, name):
+    self.name = name
+def print_list(my_list):
+"""This should be indented"""
+    print('Nope')
+def hello_world():
+  print('hello')
+    print('world')
+     # 'httpd/unix-directory'
+mimetype = 'application/x-directory'
+def start(self):
+    if True:
+#       try:
+#           self.master.start()
+#       except MasterExit:
+#           self.shutdown()
+#       finally:
+#           sys.exit()
+        self.master.start()
+    # 'httpd/unix-directory'
+mimetype = 'application/x-directory'
+if True:
+     print('Hi there')
+result = {
+   'key1': 'value',
+   'key2': 'value',
+}
+print("Python", (
+"Rules"))
+result = function_that_takes_arguments(
+    'a', 'b', 'c',
+    'd', 'e', 'f',
+    )
+result = function_that_takes_arguments('a', 'b', 'c',
+                                       'd', 'e', 'f',
+)
+if user is not None and user.is_admin or \
+    user.name == 'Grant':
+    blah = 'yeahnah'
+
+
+print("Python", (
+        "Rules"))
+print("Python", ("Hello",
+                   "World"))
+print("Python", ("Hello",
+               "World"))
+
+if (row < 0 or module_count <= row or
+    col < 0 or module_count <= col):
+    raise Exception("%s,%s - %s" % (row, col, self.moduleCount))
+my_dict = {
+    "key": "value",
+    "long": "the quick brown fox jumps over the "
+        "lazy dog",
+}
+my_list = [
+    1, 2, 3,
+    4, 5, 6,
+]
+# The space after open is unnecessary
+with open( 'file.dat') as f:
+    contents = f.read()
+with open('file.dat' ) as f:
+    contents = f.read()
+
+with open('file.dat') as f :
+    contents = f.read()
+# The space after open is unnecessary
+with open ('file.dat') as f:
+    contents = f.read()
+# There are two spaces before the multiplication operator
+num = 10
+doubled = num  * 2
+# There are two spaces after the multiplication operator
+num = 10
+doubled = num *  2
+if x    in [1, 2, 3]:
+    print(x)
+if x in [1, 2, 3]:
+    print(x)
+if age>15:
+    print('Can drive')
+age = 10+15
+x = 128<<1
+remainder = 10%2
+my_tuple = 1,2,3
+my_tuple = 1,  2
+my_tuple = 1,   2,  3
+def func(key1 = 'val1',
+         key2 = 'val2'):
+    return key1, key2
+def func(key1 = 'val1',
+         key2 = 'val2'):
+    return key1, key2
+def print_name(self):
+    print(self.name) # This comment needs an extra space
+def print_name(self):
+    print(self.name)  #This comment needs a space
+#This comment needs a space
+def print_name(self):
+    print(self.name)
+## Prints hello
+print('hello')
+def func():
+    if 1  in [1, 2, 3]:
+        print('yep!')
+def func():
+    pass
+def func():
+    if  1   in [1, 2, 3]:
+        print('yep!')
+from collections import(namedtuple, defaultdict)
+def func1():
+    pass
+def func2():
+    pass
+
+def func1():
+    pass
+
+
+
+def func2():
+    pass
+class User(object):
+    pass
+user = User()
+Anti-pattern
+
+def outer():
+    def inner():
+        pass
+import collections, os, sys
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+import sys
+Limiting the required editor window width makes it possible to have several files open side-by-side, and works well when using code review tools that present the two versions in adjacent columns
+print('Four score and seven years ago our fathers brought '\
+      'forth, upon this continent, a new nation, conceived '\
+      'in liberty, and dedicated to the proposition that '\
+      '"all men are created equal."')
+if x > 5: y = 10
+from gevent import monkey; monkey.patch_all()
+print('Hello world!');
+def f(): pass
+if var != True:
+    print("var is not equal to True")
+if var == None:
+    print("var is equal to None")
+x = True
+if x == True:
+    print('True!')
+my_list = [1, 2, 3]
+if not num in my_list:
+    print(num)
+if not user is None:
+    print(user.name)
+if type(user) == User:
+    print(user.name)
+
+try:
+    user = User.objects.get(pk=user_id)
+    user.send_mail('Hello world')
+except:
+    logger.error('An error occurred!')
+root = lambda folder_name: os.path.join(BASE_DIR, folder_name)
+
+O = 100.0
+total = O * 1.08
+if (this.isNotPython()) {
+    # Syntax error!
+}
+print("Hello world)
+def get_name(self):
+    if self.first_name and self.last_name:
+            return self.first_name + ' ' + self.last_name
+    else:
+        return self.last_name
+def first_func():
+    # The line below has two spaces after its final character
+    pass  
+def first_func():
+    pass
+      # This line contains four spaces
+
+def second_func():
+    pass
+income = (gross_wages
+          + taxable_interest)
+income = (gross_wages +
+          taxable_interest)
+my_dict = {'hello': 'world'}
+if my_dict.has_key('hello'):
+    print('It works!')
+
+def can_drive(age):
+    if age < 16:
+        raise ValueError, 'Not old enough to drive'
+    return True
+assert 'test' != 'testing'
+obj = MyObj()
+print(repr(obj))
+regex = '\.png$'
+    
+    
+class I    :
+    
+I = ""# qwe
+I(c = 6)
+# qwe
+####qwqe
+def             _generate_clone_spec    (param1=None,
+                         param2=None,
+                         param3=None,
+                         param4=False,
+                         param5=False):
+    
+   class      MyClass    (    object    ):
+    def    func1():
+           pass
+      def   func2():
+          pass
+from collections import namedtuple
+class User(object)
+:
+
+    @property
+
+    def name(self)
+    :
+        pass
+        from os import path
+
+for path in ['file1.py', 'file2.py']:
+    print(path)
+
+        
+print("qwe")
