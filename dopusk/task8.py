5c1757443afe505839b85f85bff50814846d565e
Grishin <lyshagrishin@gmail.com>
2024-03-21 16:17:55 +0300

    task9

--- dopusk/task8.py
+++ dopusk/task8.py
@@ -1,5 +1,5 @@
 def main(data):
-    d1 = ((((data[3] << 3 | data[2]) << 4) | 0) << 3) 
+    d1 = ((((data[3] << 3 | data[2]) << 4)) << 3) 
     d2 = (d1 | data[1]) << 5 | data[0]
     return hex(d2)
 
